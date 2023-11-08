48ea30507e23840df6930f8885e2133e588d4924
Rohan Sahlin <48511529+mairo2@users.noreply.github.com>
2023-11-08 17:02:12 +0100

    Update function.js

--- function.js
+++ function.js
@@ -29,3 +29,36 @@ function PlayMusic(){
     audio.play()
     play.addEventListener("click", PlayMusic())
 }
+
+function CreateDynamicForms(evt, amount){
+    // Calculate the center position for the pop-up window
+    var screenWidth = window.screen.width;
+           var screenHeight = window.screen.height;
+           var windowWidth = 600; // Adjust as needed
+           var windowHeight = 400; // Adjust as needed
+           var left = (screenWidth - windowWidth) / 2;
+           var top = (screenHeight - windowHeight) / 2;
+
+           // Create a new window
+           var popup = window.open('', 'DynamicFormsPopup', 'width=' + windowWidth + ',height=' + windowHeight + ',left=' + left + ',top=' + top);
+
+           // Write the HTML content to the new window
+           popup.document.write('<html><head><title>Dynamic Forms</title></head><body>');
+
+           // Create new forms based on the selected amount
+           for (var i = 0; i < amount; i++) {
+               var formular = document.createElement("div");
+               formular.className = "memberFormular";
+               formular.innerHTML = `
+                   <label>Namn</label><br>
+                   <input type="text" name="firstName" placeholder="förnamn"><br><br>
+                   <label>Personnummer (12 siffror)</label><br>
+                   <input type="text" name="personNummer" placeholder="00000000-0000"><br><br>
+               `;
+               popup.document.body.appendChild(formular);
+           }
+
+           // Close the HTML content in the new window
+           popup.document.write('</body></html>');
+           popup.document.close();
+}
